ee609f6b08606943e19aae88212530d01e766317
AhmedZein <ahmezain503@gmail.com>
2023-02-16 16:50:56 +0200

    adding the fading behaviour

--- Day1/JQuery-index.js
+++ Day1/JQuery-index.js
@@ -1,1 +1,18 @@
+let getImageContainer = $(".container");
+let imag = getImageContainer.find("#taksOne");
+let getButton = $("button");
+const numberOfPhoto = 7 ;
+let i = 2 ;
 
+function changePhoto () {
+    $(this).attr("src",`./img/${i}.jpg`).fadeIn(2000);
+    i++;
+    if (i > numberOfPhoto) i = 1 ; 
+    // console.log(i);
+}
+let photoInterval = setInterval(()=>{
+    imag.fadeOut(2000 , changePhoto);
+});
+getButton.on("click" , ()=>{
+    clearInterval(photoInterval);
+});
