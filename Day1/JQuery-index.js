4893f6fadbc1c9ae31dbe9e52769333d1bf6c239
AhmedZein <ahmezain503@gmail.com>
2023-02-17 22:11:15 +0200

    add stop-propagation and edit the stop button

--- Day1/JQuery-index.js
+++ Day1/JQuery-index.js
@@ -1,30 +1,30 @@
 let getImageContainer = $(".container");
 let imag = getImageContainer.find("#taksOne");
-let getButton = $("button");
+let getButton = $("#photo_button");
 const numberOfPhoto = 7 ;
 let i = 2 ;
 
 function changePhoto () {
-    $(this).attr("src",`./img/${i}.jpg`).fadeIn(2000);
+    $(this).attr("src",`./img/${i}.jpg`).fadeIn(1000);
     i++;
     if (i > numberOfPhoto) i = 1 ; 
     // console.log(i);
 }
 let photoInterval = setInterval(()=>{
-    imag.fadeOut(2000 , changePhoto);
-},2000);
+    imag.fadeOut(1000 , changePhoto);
+},1000);
 getButton.on("click" , ()=>{
     clearInterval(photoInterval);
 });
 
-
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 let imagOneList = $("#first_imag");
 let imagTwoList = $("#second_imag");
 let imagThreeList = $("#third_imag");
 
 var textToAdd = "<span>Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</span>";
 
-function checkIfTheTextExists() {
+function checkIfTheTextExists(e) {
     if  ( $(this).find("p").length === 0 ) {
         $(this).append('<p class="text_box"></p>');
         if ($(this).find(".text_box").length === 0){
@@ -38,6 +38,8 @@ function checkIfTheTextExists() {
     else {
         $(this).find("p").remove();
     }
+    console.log (e);
+    e.stopPropagation();
 }
 
 imagOneList.on("click" , checkIfTheTextExists);
