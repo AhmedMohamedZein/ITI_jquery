b1ed4c19dcf79ec84be197e9487334c60d33a48a
AhmedZein <ahmezain503@gmail.com>
2023-02-19 15:45:19 +0200

    using delegation on the add button

--- Day2/index.js
+++ Day2/index.js
@@ -1,8 +1,7 @@
-var remove = $(".remove");
-var done = $(".done");
-
-var done_button = '<button type="button" class="done">&#10004;</button>';
-var del_button = '<button type="button" class="remove"> &#10006;</button>';
+// var remove = $(".remove");
+// var done = $(".done");
+var add = $("#Add");
+var getContainer = $(".tasks-container") ;
 
 function del () {
     $(this).parent().remove();
@@ -12,6 +11,24 @@ function doneTask () {
     $(this).parent().find("span").css({"background-color":"#285430"});
 }
 
+function add_task () {
+    getContainer = $(".tasks-container") ;
+    var getText = $('input[alt="addTask"]').val();
+    var text = `<span>${getText}</span>`;
+    var done_button = '<button type="button" class="done">&#10004;</button>';
+    var del_button = '<button type="button" class="remove"> &#10006;</button>';
+    
+    getContainer.append('<div></div>'); 
+    var lastAppendedChild = getContainer.children().last();
 
-done.one('click', doneTask);
-remove.one('click', del);
+    lastAppendedChild.append(text);
+    lastAppendedChild.append(done_button);
+    lastAppendedChild.append(del_button);
+    
+}
+
+// Delegation, fire this event on the elements that have .done class and .remove class
+getContainer.on('click','.done', doneTask);
+getContainer.on('click','.remove', del);
+
+add.on('click', add_task);
